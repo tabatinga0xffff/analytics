3bbf28faca291f8b93a927c08ea5ff0faa842c13
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-03-16 13:25:22 +0800

    eh

--- lib/plausible/timezones.ex
+++ lib/plausible/timezones.ex
@@ -6,22 +6,82 @@ defmodule Plausible.Timezones do
     |> Enum.sort_by(& &1[:offset], :desc)
   end
 
-  @spec to_utc_datetime(NaiveDateTime.t(), String.t()) :: DateTime.t()
-  def to_utc_datetime(naive_date_time, timezone) do
+  @doc """
+  Represents a point in wall time as UTC.
+
+      iex> to_utc_datetime(~N[2024-03-16 09:50:45], "Asia/Kuala_Lumpur")
+      ~U[2024-03-16 01:50:45Z]
+
+  When the point in wall time is ambiguous - for instance during changing from summer to winter time -
+  the larger UTC timestamp is returned by default.
+
+      # before the CEST -> CET overlap
+      iex> to_utc_datetime(~N[2018-10-28 01:59:59], "Europe/Copenhagen")
+      ~U[2018-10-27 23:59:59Z]
+
+      # during
+      iex> to_utc_datetime(~N[2018-10-28 02:00:00], "Europe/Copenhagen")
+      ~U[2018-10-28 01:00:00Z]
+
+      iex> to_utc_datetime(~N[2018-10-28 02:00:00], "Europe/Copenhagen", :up)
+      ~U[2018-10-28 01:00:00Z]
+
+      iex> to_utc_datetime(~N[2018-10-28 02:00:00], "Europe/Copenhagen", :down)
+      ~U[2018-10-28 00:00:00Z]
+
+      iex> to_utc_datetime(~N[2018-10-28 02:30:00], "Europe/Copenhagen")
+      ~U[2018-10-28 01:30:00Z]
+
+      # after
+      iex> to_utc_datetime(~N[2018-10-28 03:00:00], "Europe/Copenhagen")
+      ~U[2018-10-28 02:00:00Z]
+
+  When there is a gap in wall time - for instance in spring when the clocks are turned forward -
+  the larger UTC timestamp (at the end of the gap) is returned by default as well.
+
+      # before the CET -> CEST gap
+      iex> to_utc_datetime(~N[2019-03-31 01:59:59], "Europe/Copenhagen")
+      ~U[2019-03-31 00:59:59Z]
+
+      # during
+      iex> to_utc_datetime(~N[2019-03-31 02:00:00], "Europe/Copenhagen")
+      ~U[2019-03-31 01:00:00Z]
+
+      iex> to_utc_datetime(~N[2019-03-31 02:30:00], "Europe/Copenhagen", :up)
+      ~U[2019-03-31 01:00:00Z]
+
+      iex> to_utc_datetime(~N[2019-03-31 02:30:00], "Europe/Copenhagen", :down)
+      ~U[2019-03-31 00:59:59.999999Z]
+
+      # after
+      iex> to_utc_datetime(~N[2019-03-31 03:00:00], "Europe/Copenhagen")
+      ~U[2019-03-31 01:00:00Z]
+
+  If the supplied time zone is invalid, the wall time is assumed to be GMT.
+
+      iex> to_utc_datetime(~N[2024-03-16 09:50:45], "Europe/Asia")
+      ~U[2024-03-16 09:50:45Z]
+
+  """
+  @spec to_utc_datetime(NaiveDateTime.t(), String.t(), :up | :down) :: DateTime.t()
+  def to_utc_datetime(naive_date_time, timezone, direction \\ :up) do
     case DateTime.from_naive(naive_date_time, timezone) do
       {:ok, date_time} ->
         DateTime.shift_zone!(date_time, "Etc/UTC")
 
-      {:gap, _before_dt, after_dt} ->
-        DateTime.shift_zone!(after_dt, "Etc/UTC")
+      {:gap, before_dt, after_dt} ->
+        DateTime.shift_zone!(pick_datetime(direction, before_dt, after_dt), "Etc/UTC")
 
-      {:ambiguous, _first_dt, second_dt} ->
-        DateTime.shift_zone!(second_dt, "Etc/UTC")
+      {:ambiguous, first_dt, second_dt} ->
+        DateTime.shift_zone!(pick_datetime(direction, first_dt, second_dt), "Etc/UTC")
 
       {:error, :time_zone_not_found} ->
         DateTime.from_naive!(naive_date_time, "Etc/UTC")
     end
   end
+
+  defp pick_datetime(:up, _, dt), do: dt
+  defp pick_datetime(:down, dt, _), do: dt
 
   @spec to_date_in_timezone(NaiveDateTime.t() | DateTime.t() | Date.t(), String.t()) :: Date.t()
   def to_date_in_timezone(dt, timezone) do
@@ -34,14 +94,20 @@ defmodule Plausible.Timezones do
 
   Examples:
 
-      iex> to_datetime_in_timezone(~U[2024-03-16 01:50:45.180928Z], "Asia/Kuala_Lumpur")
-      #DateTime<2024-03-16 09:50:45.180928+08:00 +08 Asia/Kuala_Lumpur>
+      iex> to_datetime_in_timezone(~U[2024-03-16 01:50:45Z], "Asia/Kuala_Lumpur")
+      #DateTime<2024-03-16 09:50:45+08:00 +08 Asia/Kuala_Lumpur>
 
-      iex> to_datetime_in_timezone(~N[2024-03-16 01:50:45.180928], "Asia/Kuala_Lumpur")
-      #DateTime<2024-03-16 09:50:45.180928+08:00 +08 Asia/Kuala_Lumpur>
+      iex> to_datetime_in_timezone(~N[2024-03-16 01:50:45], "Asia/Kuala_Lumpur")
+      #DateTime<2024-03-16 09:50:45+08:00 +08 Asia/Kuala_Lumpur>
 
-      iex> to_datetime_in_timezone(~N[2024-03-16 01:50:45.180928], "Etc/GMT-8")
-      #DateTime<2024-03-16 09:50:45.180928+08:00 +08 Etc/GMT-8>
+      # see https://stackoverflow.com/questions/53076575/time-zones-etc-gmt-why-it-is-other-way-round
+      iex> to_datetime_in_timezone(~N[2024-03-16 01:50:45], "Etc/GMT-8")
+      #DateTime<2024-03-16 09:50:45+08:00 +08 Etc/GMT-8>
+
+      iex> to_datetime_in_timezone(~N[2024-03-16 01:50:45], "GMT+8")
+      #DateTime<2024-03-16 09:50:45+08:00 +08 GMT+8>
+
+      # amniguous
 
   """
   @spec to_datetime_in_timezone(NaiveDateTime.t() | DateTime.t() | Date.t(), String.t()) ::
